6e56225d3218ff739cc78097e6de5359cce1a95c
Faty142 <2434528@upt.edu.mx>
2025-11-19 22:37:46 -0600

    Delete Culturita directory

--- Culturita/scripts/supabase.js
+++
@@ -1,8 +0,0 @@
-// Configuración correcta para el navegador (usando CDN)
-const supabase = window.supabase.createClient(
-  'https://gpykotfudceamuzanawa.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdweWtvdGZ1ZGNlYW11emFuYXdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwOTM4NTAsImV4cCI6MjA2OTY2OTg1MH0.dirS5jS-L3F_dbhEk1ZqfVGPdQDyn4aeoaR02fQK6sI'
-);
-
-// Opcional: Exporta para depuración
-window.supabaseClient = supabase;
